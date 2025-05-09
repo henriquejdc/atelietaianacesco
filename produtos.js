2e02a960c82d71deb8bae4c26e4863560ef440e6
henriquecorte21 <henrique.corte@neoprospecta.com>
2025-05-08 21:33:06 -0300

    feat: add new feature to improve add new items and new whatsapp messages

---
+++ produtos.js
@@ -0,0 +1,176 @@
+const produtos = [
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/aurora.png',
+      nome: 'Aurora Boreal',
+      codigo: 'telas_1',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/japao.png',
+      nome: 'Monte Fuji',
+      codigo: 'telas_2',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/lago.png',
+      nome: 'Lago',
+      codigo: 'telas_3',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/navios.png',
+      nome: 'Barcos',
+      codigo: 'telas_4',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/paisagem.png',
+      nome: 'Pequena Cidade',
+      codigo: 'telas_5',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/flores.png',
+      nome: 'Flores',
+      codigo: 'telas_6',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/flor_colorida.png',
+      nome: 'Flor Magnolia',
+      codigo: 'telas_7',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Telas',
+      imagem: 'images/works/telas/toscana.png',
+      nome: 'Pequena Vila',
+      codigo: 'telas_8',
+      descricao: 'Consulte Disponibilidade'
+    },
+  
+    // Produtos da seção "Copos e Taças"
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_1.png',
+      nome: 'Taça Vinho - Presente',
+      codigo: 'copos_1',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_2.png',
+      nome: 'Taça Vinho - Presente',
+      codigo: 'copos_2',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_3.png',
+      nome: 'Taça Champagne - Presente',
+      codigo: 'copos_3',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_4.png',
+      nome: 'Taça Champagne - Presente',
+      codigo: 'copos_4',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_5.png',
+      nome: 'Taça Champagne - Presente',
+      codigo: 'copos_5',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_6.png',
+      nome: 'Taça Vinho - Presente',
+      codigo: 'copos_6',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_7.png',
+      nome: 'Taça Gin - Presente',
+      codigo: 'copos_7',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Copos e Taças',
+      imagem: 'images/works/copos/copos_8.png',
+      nome: 'Taça Licor - Presente',
+      codigo: 'copos_8',
+      descricao: 'Consulte Disponibilidade'
+    },
+  
+    // Produtos da seção "Ornamentos"
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/chaleira.png',
+      nome: 'Ornamento Elefante',
+      codigo: 'ornamento_1',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/anjo.png',
+      nome: 'Anjo',
+      codigo: 'ornamento_2',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/mulatas.png',
+      nome: 'Mulatas',
+      codigo: 'ornamento_3',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/mulata.png',
+      nome: 'Mulata',
+      codigo: 'ornamento_4',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/africana.png',
+      nome: 'Ornamento Natalino - Bola',
+      codigo: 'Africana Incenso',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/mae_africana2.png',
+      nome: 'Mãe Africana',
+      codigo: 'ornamento_6',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/japonesa.png',
+      nome: 'Japonesa',
+      codigo: 'ornamento_7',
+      descricao: 'Consulte Disponibilidade'
+    },
+    {
+      secao: 'Ornamentos',
+      imagem: 'images/works/ornamentos/mae_africana.png',
+      nome: 'Mãe Africana',
+      codigo: 'ornamento_8',
+      descricao: 'Consulte Disponibilidade'
+    }
+  ];
+  
+  
